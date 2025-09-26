0fc625754acfd60aca340e7633fba1ef0145f05b
wangeshiizzai <keshi.yvonne84@gmail.com>
2025-09-26 09:23:35 +0300

    completed

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(books => renderBooks(books));
+}
+
+function renderBooks(books) {
+  const main = document.querySelector("main");
+  books.forEach(book => {
+    const h2 = document.createElement("h2");
+    h2.textContent = book.name;
+    main.appendChild(h2);
+  });
+}
+
